647357529d54aff6015240991e974da5cb59d0ef
vadym-fedosov <vadym.fedosov@chisw.com>
2024-07-19 15:03:34 +0300

    Added models for Realm and JSON parsing.

---
+++ promova-testtask/Models/AnimalCategory.swift
@@ -0,0 +1,8 @@
+//
+//  AnimalCategory.swift
+//  promova-testtask
+//
+//  Created by user on 19.07.2024.
+//
+
+import Foundation
---
+++ promova-testtask/Models/AnimalCategoryResponse.swift
@@ -0,0 +1,8 @@
+//
+//  AnimalCategoryResponse.swift
+//  promova-testtask
+//
+//  Created by user on 19.07.2024.
+//
+
+import Foundation
