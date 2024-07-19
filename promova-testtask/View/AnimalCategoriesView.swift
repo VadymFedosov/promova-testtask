fe39ae657266ba701311c3cafad83b725b509365
vadym-fedosov <vadym.fedosov@chisw.com>
2024-07-19 15:13:50 +0300

    Added UI part.

--- promova-testtask/View/AnimalCategoriesView.swift
+++ promova-testtask/View/AnimalCategoriesView.swift
@@ -5,4 +5,61 @@
 //  Created by user on 19.07.2024.
 //
 
-import Foundation
+import SwiftUI
+import ComposableArchitecture
+
+struct AnimalCategoriesView: View {
+    let store: Store<AppState, AppAction>
+    
+    var body: some View {
+        WithViewStore(store) { viewStore in
+            NavigationView {
+                VStack {
+                    if viewStore.isLoading {
+                        ProgressView()
+                    } else {
+                        List {
+                            ForEach(viewStore.categories) { category in
+                                HStack {
+                                    AsyncImage(url: URL(string: category.image)) { image in
+                                        image.resizable().scaledToFit()
+                                    } placeholder: {
+                                        ProgressView()
+                                    }
+                                    Text(category.title)
+                                    Text(category.description)
+                                    Text(category.status)
+                                        .foregroundColor(category.status == "paid" ? .red : .green)
+                                }
+                                .onTapGesture {
+                                    switch category.status {
+                                    case "free":
+                                        viewStore.send(.selectCategory(category))
+                                    case "paid":
+                                        viewStore.send(.showAlert(AlertState(
+                                            title: TextState("Watch Ad to continue"),
+                                            buttons: [
+                                                .cancel(TextState("Cancel")),
+                                                .default(TextState("Show Ad"), action: .send(.fetchCategories))
+                                            ]
+                                        )))
+                                    default:
+                                        viewStore.send(.showAlert(AlertState(
+                                            title: TextState("Coming Soon"),
+                                            buttons: [.default(TextState("Ok"))]
+                                        )))
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                .navigationTitle("Animal Categories")
+                .alert(
+                    store.scope(state: \.alert, action: AppAction.dismissAlert),
+                    dismiss: .dismissAlert
+                )
+            }
+        }
+    }
+}
--- promova-testtask/View/FactsView.swift
+++ promova-testtask/View/FactsView.swift
@@ -5,4 +5,53 @@
 //  Created by user on 19.07.2024.
 //
 
-import Foundation
+import SwiftUI
+import ComposableArchitecture
+
+struct FactsView: View {
+    let store: Store<AppState, AppAction>
+    
+    var body: some View {
+        WithViewStore(store) { viewStore in
+            VStack {
+                if let category = viewStore.selectedCategory {
+                    Text(category.title)
+                        .font(.largeTitle)
+                    Text(category.description)
+                        .font(.subheadline)
+                    
+                    if !viewStore.facts.isEmpty {
+                        let fact = viewStore.facts[viewStore.currentFactIndex]
+                        AsyncImage(url: URL(string: fact.image)) { image in
+                            image.resizable().scaledToFit()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        Text(fact.fact)
+                            .padding()
+                        
+                        HStack {
+                            Button("Previous") {
+                                viewStore.send(.previousFact)
+                            }
+                            Spacer()
+                            Button("Next") {
+                                viewStore.send(.nextFact)
+                            }
+                        }
+                        .padding()
+                    }
+                }
+            }
+            .navigationBarTitle(Text(viewStore.selectedCategory?.title ?? ""), displayMode: .inline)
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Back") {
+                        viewStore.send(.dismissAlert)
+                    }
+                }
+            }
+        }
+    }
+}
