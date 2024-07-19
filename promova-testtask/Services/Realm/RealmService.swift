7c14fd195f63a424e13e124f416eab9cee340892
vadym-fedosov <vadym.fedosov@chisw.com>
2024-07-19 15:07:15 +0300

    Added networking and Realm services.

--- promova-testtask/Services/Realm/RealmService.swift
+++ promova-testtask/Services/Realm/RealmService.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import RealmSwift
+
+struct RealmService {
+    private let realm = try! Realm()
+    
+    func saveCategories(_ categories: [AnimalCategoryResponse]) {
+        try? realm.write {
+            realm.delete(realm.objects(AnimalCategory.self))
+            
+            for categoryResponse in categories {
+                let category = AnimalCategory()
+                category.title = categoryResponse.title
+                category.acDescription = categoryResponse.description
+                category.image = categoryResponse.image
+                category.order = categoryResponse.order
+                category.status = categoryResponse.status
+                
+                for fact in categoryResponse.content {
+                    let factContent = FactContent()
+                    factContent.fact = fact.fact
+                    factContent.image = fact.image
+                    category.content.append(factContent)
+                }
+                
+                realm.add(category)
+            }
+        }
+    }
+    
+    func getCategories() -> Results<AnimalCategory> {
+        realm.objects(AnimalCategory.self).sorted(byKeyPath: "order", ascending: true)
+    }
+}
