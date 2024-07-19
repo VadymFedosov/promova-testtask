6b88cf2b39e8af6180535fb1514c1eb2fba01cfa
vadym-fedosov <vadym.fedosov@chisw.com>
2024-07-19 14:56:15 +0300

    Initial Commit

---
+++ promova-testtask/promova_testtaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  promova_testtaskApp.swift
+//  promova-testtask
+//
+//  Created by user on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct promova_testtaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
